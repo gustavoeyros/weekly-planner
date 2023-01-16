db6febe31458b7d1475197d68ee837c697786ac9
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-01-16 12:02:08 -0300

    feat: initial structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import RoutesManager from "./routes";
+function App() {
+  return (
+    <>
+      <RoutesManager />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import RegisterForm from "../components/RegisterForm";
+
+import { FormWrapper } from "./styled";
+const Register = () => {
+  return (
+    <FormWrapper>
+      <RegisterForm />
+    </FormWrapper>
+  );
+};
+
+export default Register;
